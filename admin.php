0378d5bc376514a6894fe63fbb4f8e8e4c27ee7f
= <=>
2019-07-18 10:33:34 +0700

    backup - animsition

--- admin.php
+++ admin.php
@@ -1,40 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> -->
-    <title>Product page</title>
-    <?php
+<?php
         include "includes/link.php";
         include ("modules/admin/productAdmin.php");
         
     ?>
-</head>
-
-<body>
-    <div id="header"></div>
-    <div class="container-fluid">
-        <div class="row mx-sm-4 my-sm-4">
-            <?php
+<div id="header"></div>
+<div class="container-fluid">
+    <div class="row mx-sm-4 my-sm-4">
+        <?php
     
   $html = loadProductTable();
   echo $html;
 
                 ?>
-        </div>
     </div>
-    <div class="container-fluid bg-light" id="footer"></div>
-
-</body>
-<?php
-include ("includes/script.php");
-?>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src="js/loadPage.js"></script>
-
-</html>
+</div>
+<div class="container-fluid bg-light" id="footer"></div>
--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <a class="nav-link" href="home.php?page=%">Home</a>
+                    <a class="nav-link" id="home" href="#">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Computer</a>
